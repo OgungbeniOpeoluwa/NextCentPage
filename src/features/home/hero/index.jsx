5057ce7dc86ab6cc4d29fe646fcd555a0040b748
Opeoluwa Ogungbeni <opeoluwaagnes@gmail.com>
2024-04-19 16:04:43 +0100

    update hero

--- src/features/home/hero/index.jsx
+++ src/features/home/hero/index.jsx
@@ -5,8 +5,8 @@ import  style from "./index.module.css"
 const Hero = ()=>{
     return(<div className={style.mainCont}>
         <div className={style.content}>
-        <div>
-            <h1 style={{color:"#4D4D4D",fontWeight:"600",fontSize:"64px"}}>Lessons and insights
+        <div className={style.heroContent}>
+            <h1>Lessons and insights
                 <span style={{display:"block" ,color:"#4CAF4F"}}>from 8 years</span></h1>
             <p>where to grow your business as a photographer: site or social media?</p>
         </div>
